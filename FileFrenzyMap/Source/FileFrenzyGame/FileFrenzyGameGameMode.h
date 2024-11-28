7ba76254e9aab9ccae0cfd4cb343e7207a70de0b
Amalia Ionescu <ionescu.amalia98@gmail.com>
2024-11-28 11:10:02 +0200

    Map layout
    Developed map layout and player movement
    
    Signed-off-by: Amalia Ionescu <ionescu.amalia98@gmail.com>

---
+++ FileFrenzyMap/Source/FileFrenzyGame/FileFrenzyGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FileFrenzyGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFileFrenzyGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFileFrenzyGameGameMode();
+};
+
+
+
